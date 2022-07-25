8ee2c10b61de0a39624f0bef3119a130e2ac8cbc
Cédric Debarle <cedric.debarle@kcitylabs.fr>
2022-07-25 11:23:45 +0200

    ajout tmtc

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ced le bg de la street'));
+app.get('/', (req, res) => res.send('Ced le bg de la street tmtc'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
